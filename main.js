24b0268ea4d87d9f3a6ecd612752749cfcc1a54b
m8a123 <43588003+m8a123@users.noreply.github.com>
2021-04-28 09:57:29 -0400

    Create main.js
    Created dictionary for corresponding words for each digit in input.

---
+++ main.js
@@ -0,0 +1,21 @@
+
+var dict = {
+    '0' : "Zero",
+    '1' : "One",
+    '2' : "Two",
+    '3' : "Three",
+    '4' : "Four",
+    '5' : "Five",
+    '6' : "Six",
+    '7' : "Seven",
+    '8' : "Eight",
+    '9' : "Nine"
+};
+
+function converter(s){
+    res = "";
+    for (var i = 0; i < s.length; i++){
+        res += dict[s[i]];
+    }
+    return res;
+}
