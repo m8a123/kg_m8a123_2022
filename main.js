8db0c05d44fdbdea12fddc7e2e85af8f8e4715f8
m8a123 <43588003+m8a123@users.noreply.github.com>
2021-04-28 10:19:33 -0400

    Used args from stdin
    Took in args from stdin, used slice function to convert args to array of the number string inputs, and mapped converter function. Tested and works.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+// corresponding word for each digit in number
 var dict = {
     '0' : "Zero",
     '1' : "One",
@@ -12,6 +13,7 @@ var dict = {
     '9' : "Nine"
 };
 
+// converts a number in string form into the phonetic equivalent
 function converter(s){
     res = "";
     for (var i = 0; i < s.length; i++){
@@ -20,4 +22,9 @@ function converter(s){
     return res;
 }
 
-console.log(converter('209'))
+// takes in arguments from command line and slices into array.
+var myArgs = process.argv.slice(2);
+// maps converter function on array of args.
+var map = myArgs.map(s => converter(s));
+
+console.log(map);
